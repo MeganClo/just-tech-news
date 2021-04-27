fc05b3267dd562e0c9b97d7a77ab107391f6f5af
Megs <Megs.close@gmail.com>
2021-04-27 11:49:02 -0700

    comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,25 +1,35 @@
 const router = require("express").Router();
-const { Post, User, Vote } = require("../../models");
+const { Post, User, Vote, Comment } = require("../../models");
 const sequelize = require('../../config/connection');
 
 router.get('/', (req, res) => {
     console.log('===========HEY===========');
     Post.findAll({
+        order: [['created_at', 'DESC']],
         attributes: [
-            'id', 
-            'post_url', 
-            'title', 
-            'created_at',
-            [sequelize.literal("(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)"), "vote_count"]
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
-        order: [[ "created_at", "DESC" ]],
         include: [
-            {
-                model: User,
-                attributes: ["username"]
+          // include the Comment model here:
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
             }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
         ]
-    })
+       })
+       
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
         console.log(err);
@@ -33,19 +43,29 @@ router.get("/:id", (req, res) => {
             id: req.params.id
         },
         attributes: [
-            "id", 
-            "post_url", 
-            "title", 
-            "created_at"
+            'id',
+            'post_url',
+            'title',
+            'created_at',
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
-        include: [
+          ],
+          include: [
+            // include the Comment model here:
             {
+              model: Comment,
+              attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+              include: {
                 model: User,
-                attributes: ["username"]
+                attributes: ['username']
+              }
+            },
+            {
+              model: User,
+              attributes: ['username']
             }
-        ]
-    })
+          ]
+         })
+         
     .then(dbPostData => {
         if(!dbPostData) {
             res.status(400).json({ message: "No post found with this id" });
