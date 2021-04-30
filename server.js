5de602a4abdcb2cc9bd9d1b9749492df835d78b3
Megs <Megs.close@gmail.com>
2021-04-29 18:03:04 -0700

    logout functionality working

--- server.js
+++ server.js
@@ -3,16 +3,30 @@ const express = require("express");
 const routes = require("./controllers/");
 const sequelize = require("./config/connection");
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3031;
 
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+  
+app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 // turn on routes
 app.use(routes);
